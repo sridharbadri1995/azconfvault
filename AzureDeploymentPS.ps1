c25c559e986bb2abb5c84ae8f043a78e5ed365fc
Learning <learning@learning.com>
2022-02-08 15:07:07 +0530

    added ARM template

---
+++ AzureDeploymentPS.ps1
@@ -0,0 +1,20 @@
+﻿Connect-AzAccount
+
+$subscriName = (Get-AzSubscription | Select-Object -First 1 | Format-Table -Property Name )  ; $subscriName
+
+Set-AzContext $subscriName
+
+$rgname = "rg-azconfvault"
+
+#New-AzResourceGroup -Location "Central US" -Name $rgname
+
+# New-AzResourceGroupDeployment -ResourceGroupName $rgname -TemplateFile $templateFile -Name addoutput
+
+New-AzDeployment -Location "Central US" -TemplateFile .\resourcegrouptemplate.json
+
+New-AzResourceGroupDeployment -Location "Central US" -ResourceGroupName $rgname -TemplateFile $templateFile .\webAppTemplate.json .\webAppTemplate.parameters.json
+
+
+
+
+
